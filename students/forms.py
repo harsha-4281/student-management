ee6b767332a1065fbb77d9bfbfd9c97c157a6f64
harsha4281 <voorcs190@rmkcet.ac.in>
2026-02-17 17:36:16 +0530

    updated UI

--- students/forms.py
+++ students/forms.py
@@ -1,7 +1,41 @@
 from django import forms
 from .models import Student
 
+
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name', 'roll_number','email', 'course','age']
+        fields = ['name', 'roll_number', 'email', 'course', 'age']
+
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter full name'
+                }
+            ),
+            'roll_number': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter roll number'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter email address'
+                }
+            ),
+            'course': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter course name'
+                }
+            ),
+            'age': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter age'
+                }
+            ),
+        }
